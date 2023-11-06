9f0bb4673e265ac4202396e3c58751ef263067f5
Veronica-Alfr <veagalves@gmail.com>
2023-11-06 20:12:52 -0300

    style: create style for page Menu

---
+++ tic-tac-toe/src/components/Board.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const Board = () => {
+  const gridSizes = [
+    { value: "3x3", label: "3x3" },
+    { value: "4x4", label: "4x4" },
+    { value: "5x5", label: "5x5" },
+    { value: "6x6", label: "6x6" },
+    { value: "7x7", label: "7x7" },
+    { value: "8x8", label: "8x8" },
+    { value: "9x9", label: "9x9" },
+    { value: "10x10", label: "10x10" },
+  ];
+
+  const [selectedSize, setSelectedSize] = useState("3x3");
+
+  const handleSizeChange = (event) => {
+    setSelectedSize(event.target.value);
+  };
+
+  return (
+    <div className="board-size">
+      <label htmlFor="gridSizeSelect">Tamanho do Tabuleiro</label>
+      <select
+        id="gridSizeSelect"
+        value={selectedSize}
+        onChange={handleSizeChange}
+      >
+        {gridSizes.map((sizeOption) => (
+          <option key={sizeOption.value} value={sizeOption.value}>
+            {sizeOption.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Board;
---
+++ tic-tac-toe/src/pages/Home/Home.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import ticTacToeLogo from "../../assets/jogo-da-velha_logo.svg";
+import "./Home.scss";
+import Board from "../../components/Board";
+
+function Home() {
+  const [opponentChoosed, setOpponentChoosed] = useState("");
+  const [match, setMatch] = useState("");
+
+  return (
+    <section className="home">
+      <header>
+        <img
+          className="tic-tac-toe-logo"
+          src={ticTacToeLogo}
+          alt="Jogo da Velha"
+        />
+      </header>
+
+      <div className="menu">
+        <div className="type-game">
+          <p className="title">Tipo de Jogo</p>
+
+          <div className="what-type">
+            <button
+              className={`opponent
+                    ${opponentChoosed === "player" && "button-active"}`}
+              onClick={() => setOpponentChoosed("player")}
+            >
+              <p>vs Jogador</p>
+            </button>
+            <button
+              className={`opponent
+                    ${opponentChoosed === "bot" && "button-active"}`}
+              onClick={() => setOpponentChoosed("bot")}
+            >
+              <p>vs Robô</p>
+            </button>
+          </div>
+        </div>
+
+        <div className="players-name">
+          <p className="title">Nome dos Jogadores</p>
+
+          <div className="input-players">
+            <input type="text" placeholder="Jogador 1" />
+            <input type="text" placeholder="Jogador 2" />
+          </div>
+        </div>
+
+        <Board />
+
+        <div className="matches">
+          <button
+            className={`start-match matches-play
+            ${match === "start" && "active"}`}
+            onClick={() => setMatch("start")}
+          >
+            <p>Começar partida</p>
+          </button>
+          <button
+            className={`historic-macthes matches-play
+            ${match === "historic" && "active"}`}
+            onClick={() => setMatch("historic")}
+          >
+            <p>Histórico de partidas</p>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
