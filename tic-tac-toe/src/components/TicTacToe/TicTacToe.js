ebfe6af9e58bcce6d0302cb7e7fe2705fba53a7b
Veronica-Alfr <veagalves@gmail.com>
2023-11-07 23:00:26 -0300

    feat: correct image of the X or O is being brought according to the player

--- tic-tac-toe/src/components/TicTacToe/TicTacToe.js
+++ tic-tac-toe/src/components/TicTacToe/TicTacToe.js
@@ -1,37 +1,51 @@
 import { useState } from "react";
+import OImg from "../../assets/o.svg";
+import XImg from "../../assets/x.svg";
 import "./TicTacToe.scss";
 
 function TicTacToe() {
   const biggerSquare = [];
 
   const player1Name = localStorage.getItem("player1");
+  const player2Name = localStorage.getItem("player2");
   const sizeBoard = localStorage.getItem("boardSize");
 
-  const [xOrO, setXorO] = useState("");
+  const [currentPlayer, setCurrentPlayer] = useState(player1Name);
 
   const sizeTotal = sizeBoard.slice(0, -2);
+
+  const handleSquareClick = () => {
+    setCurrentPlayer((prevPlayer) =>
+      prevPlayer === player1Name ? player2Name : player1Name
+    );
+  };
 
   for (let row = 0; row < sizeTotal; row++) {
     for (let col = 0; col < sizeTotal; col++) {
       const squareId = `square-${row}-${col}`;
+      const imgSrc = currentPlayer === player1Name ? OImg : XImg;
       biggerSquare.push(
         <div key={squareId} className="square">
-          <span className={`${player1Name ? "X" : "O"}`}>{xOrO}</span>
+          <div className="words" onClick={handleSquareClick}>
+            <img
+              className={`${currentPlayer === player1Name ? "O" : "X"}`}
+              src={imgSrc}
+              alt={currentPlayer === player1Name ? "O" : "X"}
+            />
+          </div>
         </div>
       );
     }
   }
 
-  // if (player1Name) {
-  //   setXorO("X");
-  // }
-
   return (
     <section className="play-tic-tac-toe">
       <h1>
         Vez do Jogador{" "}
-        <p className={`${player1Name ? "player1" : "player2"}`}>
-          {player1Name}
+        <p
+          className={`${currentPlayer === player1Name ? "player1" : "player2"}`}
+        >
+          {currentPlayer}
         </p>
       </h1>
       <div className="tic-tac-toe-board">{biggerSquare}</div>
